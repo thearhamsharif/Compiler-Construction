73650ff7267fb1d2247deb11aac24a744cdb8b04
Arham Sharif <sarham928@gmail.com>
2023-11-04 19:00:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ TOKEN_TYPES = [
     ('KEYWORD', r'(Main|func|let|var|static|for|while|if|else if|elif|else|concat|pow|sqrt|class|init|deinit|public|protected|private|super|abstract|this|is_int|is_char|is_float|is_bool|is_str|replace|find|len)'),
     ('DATATYPE', r'(int|float|char|str|bool)'),
     ('CHAR', r'"[^"]{1}"|\'[^\']{1}\''),
-    ('STRING', r'"[^"]"|\'[^\']\''),
+    ('STRING', r'"[^"]*"|\'[^\']*\''),
     ('IDENTIFIER', r'[a-zA-Z_][a-zA-Z0-9_]*'),
     ('FLOAT', r'\b[0-9]+\.[0-9]+\b'),
     ('INT', r'\b[0-9]+\b'),
     ('NULL', r'null'),
     ('BOOLEAN', r'(true|false)'),
-    ('COMMENT', r'(//.|/\(.|\n)\/)'),
+    ('COMMENT', r'(//.*|/\*(.|\n)*\*/)'),
     ('OPERATOR',
-     r',|=>|;|\(|\)|\{|\}|\:|\.|\+=|-=|\=|\+\+|--|&&|\|\||===|==|!==|!=|<=|>=|\\|\+|-|\|/|>|<|=')
+     r',|=>|;|\(|\)|\{|\}|\:|\.|\+=|-=|\*=|\+\+|--|&&|\|\||===|==|!==|!=|<=|>=|\*\*|\+|-|\*|/|>|<|=')
 ]
 
 
@@ -29,17 +29,19 @@ def tokenize(source_code):
                 if token_type == 'OPERATOR':
                     tokens.append((value, value, line_no))
                 else:
+                    # if token_type == "STRING":
                     tokens.append((token_type, value, line_no))
-                source_code = source_code[len(value):].lstrip()
+                source_code = source_code[len(value):]
 
                 break
-        else:
-            raise SyntaxError(f"Unexpected character: {source_code[0]}")
+            else:
+                tokens.append(("Unexpected character", source_code[0], line_no))
+                # source_code = source_code[len(source_code[0])]
 
     return tokens
 
 
-if _name_ == '_main_':
+if __name__ == '__main__':
 
     with open("mySourceCode", "r") as file:
         source_code = file.read()
